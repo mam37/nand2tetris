5c8094ce922bb7fd5321cab79682a90af68e29f6
mike <mike@localhost.localdomain>
2018-01-16 21:37:31 -0500

    use input file to name output file

--- projects/07/VMTranslator.py
+++ projects/07/VMTranslator.py
@@ -1,25 +1,31 @@
 from Parser import Parser
 from CodeWriter import CodeWriter
-import argparse
+import argparse, os
 
+def translateFile(parser, codeWriter):
+    parser.reset()
+    while parser.hasMoreCommands():
+        parser.advance()
+        cmd = parser.commandType()
+        print cmd
+        if cmd == 'push':
+            codeWriter.writePushPop(cmd, parser.arg1(), parser.arg2())
+        elif cmd == 'add':
+            codeWriter.writeArithmetic(cmd)
 
 argparser = argparse.ArgumentParser()
-argparser.add_argument('infile')
-#argparser.add_argument('outfile')
+argparser.add_argument('src')
 args = argparser.parse_args()
 
-infile = open(args.infile)
-parser = Parser(infile)
-outfile = open('out.hack', 'w')
-codeWriter = CodeWriter(outfile)
 
-while parser.hasMoreCommands():
-    parser.advance()
-    cmd = parser.commandType()
-    print cmd
-    if cmd == 'push':
-        codeWriter.writePushPop(cmd, parser.arg1(), parser.arg2())
-    elif cmd == 'add':
-        codeWriter.writeArithmetic(cmd)
+if os.path.isfile(args.src):
+    src = open(args.src)
+    destPath = os.path.splitext(args.src)[1] + '.asm' 
+    outfile = open(destPath, 'w')
+    
+    parser = Parser(src)
+    codeWriter = CodeWriter(outfile)
+    translateFile(parser, codeWriter)
+
 
 codeWriter.close()
